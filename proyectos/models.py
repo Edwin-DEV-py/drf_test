7a146024b0e0e3534f7c7a5d44e783fd76052c88
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-04-26 00:06:59 -0500

    autor

--- proyectos/models.py
+++ proyectos/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Proyectos(models.Model):
     id = models.AutoField(primary_key=True)
+    autor = models.IntegerField(default=0)
     titulo = models.CharField(max_length=100)
     descripcion = models.CharField(max_length=255)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
