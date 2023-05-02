648b06963dc02d34f5d0b5eef03c297b171a077e
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-05-02 02:09:25 -0500

    funciona!

--- rest/settings.py
+++ rest/settings.py
@@ -135,3 +135,5 @@ if not DEBUG:
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
