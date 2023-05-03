3a375740795c93808ec4fa9096d4b57e2141fe02
edwin-tarazona <80715680+Edwin-Papasito@users.noreply.github.com>
2023-05-03 01:25:21 -0500

    prueba

--- proyectos/api.py
+++ proyectos/api.py
@@ -2,12 +2,31 @@
 #listar, crear, devolver, actualiza o borrar un objeto.
 #Ademas podemos definir quien puede consultar esto yque peticiones se pueden hacer 
 #y enviar autentificaciones.
+from django.http import HttpResponse ##prueba
 
 from .models import *
 from rest_framework import viewsets,permissions
 from .serializers import *
 
+response = HttpResponse() ##prueba
+response['Access-Control-Allow-Origin'] = 'http://localhost:5173' ##prueba
+
+
 class CrudApi(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
     permission_classes = [permissions.AllowAny]
+    
+    ##prueba listar
+    def list(self, request, *args, **kwargs):
+        response = super(CrudApi, self).list(request, *args, **kwargs)
+        response["Access-Control-Allow-Origin"] = "http://localhost:5173"
+        return response
+    
+    ##prueba update
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=kwargs.pop('partial', False))
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
